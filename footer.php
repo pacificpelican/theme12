d6e67bcf8dc6385327b99c594f5b2485c6f3e9b8
Daniel McKeown <2014@pacificpelican.us>
2015-11-18 00:23:16 -0800

    Theme 12 0.2.0-beta3 styling updates

--- footer.php
+++ footer.php
@@ -17,7 +17,9 @@
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'theme-12' ) ); ?>"><?php printf( __( 'Powered by %s', 'theme-12' ), 'WordPress' ); ?></a>
 			<span class="sep"> & </span>
 		<!--	Theme: <a class="grey-text text-lighten-3" href="<?php $my_theme = wp_get_theme(); echo $my_theme->get( 'ThemeURI' ); ?>"><?php echo wp_get_theme(); ?></a> version <?php $my_theme = wp_get_theme(); echo $my_theme->get( 'Version' ); ?> -->
-			<?php printf( esc_html__( '%1$s  %2$s.', '' ), '', '<a href="http://djmcloud.danieljmckeown.com/theme11">Theme 11</a> by <a href="http://danmckeown.info/" rel="designer">Dan McKeown</a>' ); ?>
+			<?php echo "<a href='http://djmcloud.danieljmckeown.com/theme12'>Theme 12</a> "; ?>
+			<?php $my_theme = wp_get_theme(); echo "v" . $my_theme->get( 'Version' ); ?>
+			<?php echo " by <a href='http://danmckeown.info/' rel='designer'>Dan McKeown</a>"; ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
